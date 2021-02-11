646fb2a385dbe96fbb4a45f7bf282002211cf94c
Lalith Kishore <lalithk3991@gmail.com>
2021-02-12 02:13:15 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div class="container">
+      <h1>Hello world!</h1>
+      <p>Have fun with this boilerplate maga...</p>
+    </div>
+  )
+}
